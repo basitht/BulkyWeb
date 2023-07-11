0f9bd4b0ce6c4ddba5c3fed963bee484bacca92a
Basiyath Basiyath <basiyath.basiyath@contractors.accruent.com>
2023-07-11 12:15:28 +0530

    Add applicationdb

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -3,7 +3,7 @@ using BulkyWeb.Models;
 
 namespace BulkyWeb.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : DbContext 
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
